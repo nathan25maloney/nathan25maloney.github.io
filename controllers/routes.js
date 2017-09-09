c452057bae82232d30b1f8c018c246d4fc2f1f77
Nathan Maloney <nathan25maloney@gmail.com>
2017-09-08 17:15:30 -0700

    starting to add actual content to the site

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,29 +1,10 @@
 
  
-module.exports = function(app, passport) {
+module.exports = function(app) {
 
     app.get('/', function(req,res){
         res.render('index.handlebars');
     });
-
-    app.get('/about', function(req, res) {
-        res.render('about.handlebars');
-    });
-
-    app.get('/portfolio', function(req, res) {
-        res.render('portfolio.handlebars');
-    });
-
-    app.get('/contact', function(req, res) {
-        res.render('contact.handlebars');
-    });
- 
- 
- 
-
-
-
-
 
  
 }
--- server.js
+++ server.js
@@ -9,13 +9,10 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
-
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -24,25 +21,15 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
-
-
-
-
 // Import routes and give the server access to them.
 
 
 require('./controllers/routes.js')(app);
 
 
-
-
-
-
-
-
-  app.listen(port, function() {
+app.listen(port, function() {
     console.log("App listening on PORT " + port);
-  });
+});
 
 
 
